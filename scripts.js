6db918ebdc641854eae9dbc810e10f96df661c63
Eric Rook <ericrook@knights.ucf.edu>
2013-10-29 11:29:57 -0400

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+$(document).ready(function() {
+	
+});
+
+function test(movement){
+	if(movement > 2){
+		var checkOne = true;
+	}
+	else if(movement  < -2){
+		var checkTwo = true;	
+	}
+	
+	
+}
