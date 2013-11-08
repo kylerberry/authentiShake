6b96bd192078205c81a5aa18a6d96184f3d24acc
Kyler Berry <kyler.berry@gmail.com>
2013-11-08 10:31:53 -0500

    got input form ready for submission, also wrote lots of psuedo code and set up a couple of javascript functions

--- scripts.js
+++ scripts.js
@@ -1,27 +1,38 @@
 // JavaScript Document
 
 $(document).ready(function() {
-	$('#button-blue').on('click',function(e) {
-		e.preventDefault();
-		$('#shake').toggle();
-		window.setTimeout(function(){
-			$('#shake').toggle();
-			$('#submitGood').toggle();
-			window.setTimeout(function(){
-				$('#submitGood').toggle();
-			}, 1500);
-		}, 3000);
-		
-	});
-});
+	
+	/* PSUEDO */
+	//intercept submit input
+	//check to make sure the honeypot wasn't tripped
+	//have an array of different motions to send the user
+	//display a 'motion' to mimic with matches an array of acceptable responses
+		//there needs to be ranges, because user input will be dirty, not perfect
+		//may need to first show a modal that tells them to calibrate their phone (their phone may need to be in a starting position)
+	//capture user inputs, perhaps using snapshots at timed intervals
+	//if inputs are acceptable
+		//capture input fields
+		//submit fields
+		//send a message acknowledging the user's human-ness
+	//otherwise they fail
+		//sent a modal indicating that they didn't pass
+		//try again or choose to use regular captcha
 
-function test(movement){
-	if(movement > 2){
-		var checkOne = true;
+	function get_pattern(e) {
+		/*this will see the submit request
+		fetch a pattern of movement
+		then call the display modal function*/
 	}
-	else if(movement  < -2){
-		var checkTwo = true;	
+
+	function choose_modal(message) {
+		/* get the parameter that shows us which modal to show */
+		//this just sets the default value of our parameter to null if it isn't set by the function calling it
+		message = typeof message !== 'undefined' ? message : null;
+
+		/*check for message an display appropriate modal to the user: 
+		null, show pattern, movement captured(checking), movement not captured(try again), verified a person(Success), not a human(fail) 
+
+		create a display function for each of these */
 	}
-	
-	
-}
+
+});
