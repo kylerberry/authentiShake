9e83d4358e782db94df182f75fd8db5f473f57ba
Kyler Berry <kyler.berry@gmail.com>
2013-11-20 01:25:36 -0500

    - honeypot bot checking works - submit detects if captcha authentication is being used or if our default method is used - wrote function that logs more accurate data to screen - commented on possible capturing patterns as well as some observations I made looking at the data - fixed some responsive styles

--- scripts.js
+++ scripts.js
@@ -1,42 +1,129 @@
-// JavaScript Document
+/* -------- Thought Process -------- */
+
+/* intercept submit input
+	check to make sure the honeypot wasn't tripped
+	have an array of different motions to send the user
+	display a 'motion' to mimic with matches an array of acceptable responses
+		there needs to be ranges, because user input will be dirty, not perfect
+		may need to first show a modal that tells them to calibrate their phone (their phone may need to be in a starting position)
+	capture user inputs, perhaps using snapshots at timed intervals
+	if inputs are acceptable
+		capture input fields
+		submit fields
+		send a message acknowledging the user's human-ness
+	otherwise they fail
+		sent a modal indicating that they didn't pass
+		try again or choose to use regular captcha */
+
+
+
+/* -------- CAPTURE PATTERNS, DATA OBSERVATIONS -------- */
+
+//Mark suggests running checks at a 100ms instead of 500ms
+
+//create a visual that represents each raw data iteration, output it to the page
+// - could even make a cool data chart for different device readings at the different positions a person might hold a phone
+
+// If a user is looking at their phone. BETA Orientation should ALWAYS be negative. (need to check if thats true on orientation change)
+
+//might need to include deviceMovement just to determine moving UP and DOWN in 3d space (if you want to shake the phone)
+
+//could reduce it to movements, pattern is 4 'step', coach them through every step. 'snapshot' at the end of each prompt. SHOULD guarantee values, but slower process
+
+// **** check the value, don't record it until the value changes direction, compare our 'snapshots' for positive vs. negative pattern.
+// **** can also be done with x,y,z values!!
+
+
 
 $(document).ready(function() {
+	//determine if user has opted for captcha
 	var flag = false;
-	//check to see if acceleromter/gyro data is not null
-	if(flag == false){
-		showRecaptcha();
-		$('#form1').attr('action', 'verify.php');
-		
-		flag == true;
+	var kill = false; // just an easy way to kill my gyro tracking function
+
+	//click handler
+	jQuery('form').on( 'click', '#submit', function(event) { on_submit(event); } );
+
+	//kill button
+	jQuery('body').on( 'click', '#kill', function() { kill = true; } );
+
+	//if a person prefers captcha
+	jQuery('form').on( 'click', '#opt-out', function(event) {
+		flag = true;
+		showRecaptcha(event);
+	} );
+
+	//function called when submit is clicked
+	//sets is_human to true if the trap was not tripped
+	//if submission is not a bot then proceed
+	function on_submit(event) {
+
+		var is_human = trap_tripped();
+		var is_captcha = (flag === true) ? true : false ;
+		console.log("flag: " + flag);
+
+		if(is_human)
+		{
+			console.log('You\'re Human');
+			if(is_captcha)
+			{
+				console.log('Verify Captcha');
+				$('#form').attr('action', 'verify.php');
+			} else {
+				event.preventDefault();
+				var pattern = get_pattern(event);
+			}
+		}
+		else console.log('You\'re a bot. DIE.');
 	}
-	function showRecaptcha(e) {
+
+	//check honeypot
+	//returns true or false
+	function trap_tripped() {
+		var honey = jQuery('#honeypot').val();
+		return ( honey === '' ) ? true : false;
+	}
+
+	//this retrieves our pattern to display to the user and to test against
+	function get_pattern(event) {
+		console.log('Get Pattern');
+		
+		var $output = jQuery('#gyro-output');
+		var $gyro_message = '';
+		var $accel_message = '';
+		var temp = '';
+		var time = 0;
+
+		//o.x, o.y, o.z for accelerometers
+		//o.alpha, o.beta, o.gamma for gyro
+		gyro.startTracking(function(o) {
+
+			if( kill === true ) gyro.stopTracking();
+
+			time += 0.5;
+
+			//console.log( 'x motion: ' + o.x + '\n' + 'y motion: ' + o.y + '\n'+ 'z motion: ' + o.z );
+			$accel_message = '<span>x: </span>' + o.x + '<br/>' + '<span>y: </span>' + o.y + '<br/>' + '<span>z: </span>' + o.z + '<br/><br/>';
+			$gyro_message = $accel_message + '<span>alpha: </span>' + o.alpha + '<br/>' + '<span>beta: </span>' + o.beta + '<br/>' + '<span>gamma: </span>' + o.gamma + '<br/>' + '<span>time: </span>' + time + 's<hr/>';
+			
+			//logs my outputs to the page so i can debug easier on phone
+			$output.prepend($gyro_message);
+
+		});
+
+		
+		/*this will see the submit request
+		fetch a pattern of movement
+		then call the display modal function*/
+	}
+
+	//this function displays our captcha as a backup if authentiShake fails
+	function showRecaptcha(event) {
+		event.preventDefault();
 		Recaptcha.create("6Le6ieoSAAAAAD91UUIcUK-BWqHAqKtuQfcYRz7s", 'captchadiv', {
 			tabindex: 1,
 			theme: "red",
 			callback: Recaptcha.focus_response_field
 		});
-	  }
-	
-	/* PSUEDO */
-	//intercept submit input
-	//check to make sure the honeypot wasn't tripped
-	//have an array of different motions to send the user
-	//display a 'motion' to mimic with matches an array of acceptable responses
-		//there needs to be ranges, because user input will be dirty, not perfect
-		//may need to first show a modal that tells them to calibrate their phone (their phone may need to be in a starting position)
-	//capture user inputs, perhaps using snapshots at timed intervals
-	//if inputs are acceptable
-		//capture input fields
-		//submit fields
-		//send a message acknowledging the user's human-ness
-	//otherwise they fail
-		//sent a modal indicating that they didn't pass
-		//try again or choose to use regular captcha
-
-	function get_pattern(e) {
-		/*this will see the submit request
-		fetch a pattern of movement
-		then call the display modal function*/
 	}
 
 	function choose_modal(message) {
